c99584850a32e203305811ff6508475ee9fe487e
Connor Hood <connor_hood@yahoo.com>
2021-04-20 12:31:33 -0500

    finished typing battle logic, will test later

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from player import Player
 
+
 class Game:
     def __init__(self):
         self.player_one = Player()
@@ -10,38 +11,61 @@ class Game:
         self.player_one.person_or_computer()
         self.player_one.select_choice()
 
-
     def battle(self, player_choice, opponent_choice):
         self.player_one.chosen_gesture = player_choice
         self.player_two.chosen_gesture = opponent_choice
         if player_choice == 0 and opponent_choice == 2:
-            print("Rock crushes Scissors!")
+            print(f'Rock crushes Scissors! \n {self.player_one.name} wins!')
             self.player_one.score += 1
         elif player_choice == 0 and opponent_choice == 3:
-            print("Rock crushes Lizard!")
+            print(f'Rock crushes Lizard! \n {self.player_one.name} wins!')
             self.player_one.score += 1
         elif player_choice == 1 and opponent_choice == 0:
-            print("Paper covers Rock!")
+            print(f'Paper covers Rock! \n {self.player_one.name} wins!')
             self.player_one.score += 1
         elif player_choice == 1 and opponent_choice == 4:
-            print("Paper disproves Spock!")
+            print(f'Paper disproves Spock! \n {self.player_one.name} wins!')
             self.player_one.score += 1
         elif player_choice == 2 and opponent_choice == 1:
-            print("Scissors cut Paper!")
+            print(f'Scissors cut Paper! \n {self.player_one.name} wins!')
             self.player_one.score += 1
         elif player_choice == 2 and opponent_choice == 3:
-            print("Scissors decapitates Lizard")
+            print(f'Scissors decapitates Lizard \n {self.player_one.name} wins!')
             self.player_one.score += 1
         elif player_choice == 3 and opponent_choice == 4:
-            print("Lizard poisons Spock")
+            print(f'Lizard poisons Spock \n {self.player_one.name} wins!')
             self.player_one.score += 1
         elif player_choice == 3 and opponent_choice == 1:
-            print("Lizard eats paper")
+            print(f'Lizard eats paper \n {self.player_one.name} wins!')
             self.player_one.score += 1
         elif player_choice == 3 and opponent_choice == 4:
-            print("Lizard poisons Spock")
+            print(f'Lizard poisons Spock \n {self.player_one.name} wins!')
             self.player_one.score += 1
-
-
-
-
+        # break for separation of players
+        if opponent_choice == 0 and player_choice == 2:
+            print(f'Rock crushes Scissors! \n {self.player_two.name} wins!')
+            self.player_two.score += 1
+        elif opponent_choice == 0 and player_choice == 3:
+            print(f'Rock crushes Lizard! \n {self.player_two.name} wins!')
+            self.player_two.score += 1
+        elif opponent_choice == 1 and player_choice == 0:
+            print(f'Paper covers Rock! \n {self.player_two.name} wins!')
+            self.player_two.score += 1
+        elif opponent_choice == 1 and player_choice == 4:
+            print(f'Paper disproves Spock! \n {self.player_two.name} wins!')
+            self.player_two.score += 1
+        elif opponent_choice == 2 and player_choice == 1:
+            print(f'Scissors cut Paper! \n {self.player_two.name} wins!')
+            self.player_two.score += 1
+        elif opponent_choice == 2 and player_choice == 3:
+            print(f'Scissors decapitates Lizard \n {self.player_two.name} wins!')
+            self.player_two.score += 1
+        elif opponent_choice == 3 and player_choice == 4:
+            print(f'Lizard poisons Spock \n {self.player_two.name} wins!')
+            self.player_two.score += 1
+        elif opponent_choice == 3 and player_choice == 1:
+            print(f'Lizard eats paper \n {self.player_two.name} wins!')
+            self.player_two.score += 1
+        elif opponent_choice == 3 and player_choice == 4:
+            print(f'Lizard poisons Spock \n {self.player_two.name} wins!')
+            self.player_two.score += 1
